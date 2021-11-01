ab9ac254be1cbeb468273457af8c996d6cb9a4c3
matthewoots <matthewoots@gmail.com>
2021-11-01 19:00:20 +0800

    Success in connecting with PX4 and also started EKF fusion, will continue to debug, added new scene and skybox from [RWS by YuriyGulyak] simulator

--- Scripts/SensorSuite.cs
+++ Scripts/SensorSuite.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -41,16 +42,21 @@ public class SensorSuite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lvel = getLocalVel();
-        lacc = getLocalAcc();
         update_sensor_data();
     }
 
     void update_sensor_data()
     {
         Vector3 gpose = RUFtoFRD(transform.position);
-        gps = gps_pose2coord(gpose, home, home_alt);
+        Vector2 gps_rad = gps_pose2coord(gpose, home, home_alt);
+        gps = new Vector2(gps_rad.x * (float)(180 / MathUtils.M_PI), gps_rad.y * (float)(180 / MathUtils.M_PI));
         // Debug.Log("gps : " + gps);
+        // Debug.Log("rotation q : " + rb.rotation);
+
+        lvel = getLocalVel();
+        lacc = getLocalAcc();
+
+        Quaternion q_ned = QuaternionRUFLH2NEDRH(transform.rotation);
 
         // IMPORTANT The Sensors are right-handed.  Unity is left handed.
         // Axis Alignment = RUF to FRD
@@ -58,14 +64,13 @@ public class SensorSuite : MonoBehaviour
 
 
         // ------- SENSORS --------
-
-        ulong ut = (ulong)(Time.time * 10e6);
+        ulong ut = (ulong)(Time.time * 1e6);
         _sensor_data.time_usec = ut;
 
         // we must flip all the rotations since we are using right hand not left hand (-ve)
-        _sensor_data.xacc = RUFtoFRD(getLocalAcc()).x;
-        _sensor_data.yacc = RUFtoFRD(getLocalAcc()).y;
-        _sensor_data.zacc = RUFtoFRD(getLocalAcc()).z;
+        _sensor_data.xacc = rng(RUFtoFRD(getLocalAcc()).x, 0.01f);
+        _sensor_data.yacc = rng(RUFtoFRD(getLocalAcc()).y, 0.01f);
+        _sensor_data.zacc = rng(RUFtoFRD(getLocalAcc()).z - MathUtils.G, 0.01f);
 
         _sensor_data.xgyro = RUFtoFRD(getLocalAngVel()).x;
         _sensor_data.ygyro = RUFtoFRD(getLocalAngVel()).y;
@@ -73,18 +78,21 @@ public class SensorSuite : MonoBehaviour
 
         _sensor_data.fields_updated = (uint)SensorSource.ACCEL | (uint)SensorSource.GYRO;
 
-        _sensor_data.temperature = getTempLocal(transform.position.y);
-        _sensor_data.abs_pressure = getAbsolutePressure(transform.position.y);
-        _sensor_data.pressure_alt = getPressureAltitude(transform.position.y);
+        _sensor_data.temperature = rng(getTempLocal(-transform.position.y), 2.0f);
+        _sensor_data.abs_pressure = rng(getAbsolutePressure(-transform.position.y), 2.0f);
+        _sensor_data.pressure_alt = rng(getPressureAltitude(-transform.position.y), 0.05f);
+        // Debug.Log("baro alt : " + _sensor_data.pressure_alt);
+
         _sensor_data.fields_updated = _sensor_data.fields_updated | (uint)SensorSource.BARO;
 
-        // _sensor_data.xmag = xmag;
-        // _sensor_data.ymag = ymag;
-        // _sensor_data.zmag = zmag;
-        // _sensor_data.abs_pressure = abs_pressure;
+        Vector3 mag = getMag(gps);
+        _sensor_data.xmag = mag.x;
+        _sensor_data.ymag = mag.y;
+        _sensor_data.zmag = mag.z;
+
+        _sensor_data.fields_updated = _sensor_data.fields_updated | (uint)SensorSource.MAG;
+
         // _sensor_data.diff_pressure = diff_pressure;
-        // _sensor_data.pressure_alt = pressure_alt;
-        // _sensor_data.temperature = temperature;
         // _sensor_data.id = id;
 
         // ignition::math::Quaterniond q_nb = q_ENU_to_NED * q_gr * q_FLU_to_FRD.Inverse();
@@ -96,16 +104,22 @@ public class SensorSuite : MonoBehaviour
 
         // ------- QUATERNION STATE --------
         _state_data.time_usec = ut;
+        float[] attitude = new float[4];
+        attitude[0] = q_ned.w;
+        attitude[1] = q_ned.x;
+        attitude[2] = q_ned.y;
+        attitude[3] = q_ned.z;
 
-        // _state_data.attitude_quaternion = attitude_quaternion;
+        _state_data.attitude_quaternion = attitude;
         
-        // _state_data.rollspeed = rollspeed;
-        // _state_data.pitchspeed = pitchspeed;
-        // _state_data.yawspeed = yawspeed;
+        _state_data.rollspeed = getLocalNEDAngVel().x;
+        _state_data.pitchspeed = getLocalNEDAngVel().y;
+        _state_data.yawspeed = getLocalNEDAngVel().z;
         
-        _state_data.lat = (int)(gps.x * 180 / MathUtils.M_PI * 1e7);
-        _state_data.lon = (int)(gps.y  * 180 / MathUtils.M_PI * 1e7);
-        _state_data.alt = (int)((-transform.position.y - home_alt) * 1000);
+        _state_data.lat = (int)(gps_rad.x * MathUtils.rad2deg * 1e7);
+        _state_data.lon = (int)(gps_rad.y  * MathUtils.rad2deg * 1e7);
+        _state_data.alt = (int)((transform.position.y - home_alt) * 1000);
+        // Debug.Log("gps alt : " + _state_data.alt);
 
         _state_data.vx = (short)(RUFtoFRD(getLocalVel()).x * 100);
         _state_data.vy = (short)(RUFtoFRD(getLocalVel()).y * 100);
@@ -127,9 +141,9 @@ public class SensorSuite : MonoBehaviour
         _gps_data.time_usec = ut;
 
         _gps_data.fix_type = (byte)3;
-        _gps_data.lat = (int)(gps.x * 180 / MathUtils.M_PI * 1e7);;
-        _gps_data.lon = (int)(gps.y  * 180 / MathUtils.M_PI * 1e7);
-        _gps_data.alt = (int)((-transform.position.y - home_alt) * 1000);
+        _gps_data.lat = (int)(gps_rad.x * 180 / MathUtils.M_PI * 1e7);
+        _gps_data.lon = (int)(gps_rad.y  * 180 / MathUtils.M_PI * 1e7);
+        _gps_data.alt = (int)((transform.position.y - home_alt) * 1000);
         _gps_data.eph = (ushort)(std_xy_ * 100.0);
         _gps_data.epv = (ushort)(std_z_ * 100.0);
 
@@ -150,7 +164,14 @@ public class SensorSuite : MonoBehaviour
         Vector3 lacc = (lvel - prev_lvel)/Time.deltaTime;
         // Debug.Log("local acceleration : " + lacc);
         prev_lvel = lvel;
-        return transform.rotation * lacc;
+        return lacc;
+    }
+
+    Vector3 getLocalNEDAcc()
+    {
+        Vector3 lnedacc = QuaternionRUFLH2NEDRH(new Quaternion(0,0,0,1)) * getLocalAcc();
+        // Debug.Log("local NED acceleration : " + lnedacc);
+        return lnedacc;
     }
 
     Vector3 getGloballAcc()
@@ -176,6 +197,13 @@ public class SensorSuite : MonoBehaviour
         return transform.InverseTransformDirection(rb.angularVelocity);
     }
 
+    Vector3 getLocalNEDAngVel()
+    {
+        Vector3 lnedavel = QuaternionRUFLH2NEDRH(new Quaternion(0,0,0,1)) * getLocalAngVel();
+        // Debug.Log("local NED angular velocity : " + lnedavel);
+        return lnedavel;
+    }
+
     Vector2 gps_heading2distance(float lat_start, float lon_start, float bearing, float dist)
     {
         bearing = MathUtils.wrap_2pif(bearing);
@@ -194,7 +222,8 @@ public class SensorSuite : MonoBehaviour
 
     Vector2 gps_pose2coord(Vector3 pos, Vector2 home, float alt_home)
     {
-        float lat_home = home.x; float lon_home = home.y;
+        Vector2 home_rad = new Vector2(home.x * (float)MathUtils.deg2rad, home.y * (float)MathUtils.deg2rad);
+        float lat_home = home_rad.x; float lon_home = home_rad.y;
         // reproject local position to gps coordinates
         float x_rad = pos.y / MathUtils.CONSTANTS_RADIUS_OF_EARTH_F;    // north
         float y_rad = pos.x / MathUtils.CONSTANTS_RADIUS_OF_EARTH_F;    // east
@@ -225,6 +254,7 @@ public class SensorSuite : MonoBehaviour
         float pressure_ratio = Mathf.Pow(temperature_msl / temperature_local, 5.256f);
         float pressure_msl = 101325.0f; // pressure at MSL
         float absolute_pressure = pressure_msl / pressure_ratio;
+        absolute_pressure *=  0.01f;
         return absolute_pressure;
     }
     
@@ -237,7 +267,7 @@ public class SensorSuite : MonoBehaviour
         float density_ratio = Mathf.Pow(temperature_msl / temperature_local, 4.256f);
         float rho = 1.225f / density_ratio;
 
-        return alt_msl / (3 * rho);
+        return alt_msl / (1 * rho);
         // can add noise abs_pressure_noise + baro_drift_pa_
         // gravity_W_.Length() = 3 since Vector3
     }
@@ -250,5 +280,52 @@ public class SensorSuite : MonoBehaviour
         float temperature_local = temperature_msl - lapse_rate * alt_msl;
         return temperature_local - 273.0f;
     }
+
+    float rng(float mean, float max)
+    {
+        System.Random random = new System.Random();
+        // Returns a random number bw -1.0 and 1.0
+        return ((float)(random.NextDouble()*2) - 1.0f) * max + mean;
+    }
     
+
+    private Quaternion QuaternionRUFLH2NEDRH(Quaternion q_in)
+    {
+        float tmpQuatAngle;
+        Vector3 quatAxis;
+        q_in.ToAngleAxis(out tmpQuatAngle, out quatAxis);
+        float quatAngle = tmpQuatAngle * (float)MathUtils.deg2rad;
+        quatAxis.Normalize();
+        quatAngle *= -1;
+        quatAxis = new Vector3(quatAxis.z, quatAxis.x, -quatAxis.y);
+        
+        float s = Mathf.Sin(quatAngle / 2);
+        float quatX = quatAxis.x * s;
+        float quatY = quatAxis.y * s;
+        float quatZ = quatAxis.z * s;
+        float quatW = Mathf.Cos(quatAngle / 2);
+        Quaternion q_out = new Quaternion(quatW, quatX, quatY, quatZ);
+        return q_out;
+    }
+
+    private Vector3 getMag(Vector2 gps)
+    {
+        Vector2 home_rad = new Vector2(gps.x * (float)MathUtils.deg2rad, gps.y * (float)MathUtils.deg2rad);
+        float groundtruth_lat_rad_ = home_rad.x; float groundtruth_lon_rad_ = home_rad.y;
+
+        // Magnetic declination and inclination (radians)
+        float declination_rad = MathUtils.get_mag_declination(groundtruth_lat_rad_ * (float)MathUtils.rad2deg, groundtruth_lon_rad_ * (float)MathUtils.rad2deg) * (float)MathUtils.deg2rad;
+        float inclination_rad = MathUtils.get_mag_inclination(groundtruth_lat_rad_ * (float)MathUtils.rad2deg, groundtruth_lon_rad_ * (float)MathUtils.rad2deg) * (float)MathUtils.deg2rad;
+
+        // Magnetic strength (10^5xnanoTesla)
+        float strength_ga = 0.01f * MathUtils.get_mag_strength(groundtruth_lat_rad_ * (float)MathUtils.rad2deg, groundtruth_lon_rad_ * (float)MathUtils.rad2deg);
+
+        // Magnetic filed components are calculated by http://geomag.nrcan.gc.ca/mag_fld/comp-en.php
+        float H = strength_ga * Mathf.Cos(inclination_rad);
+        float Z = Mathf.Tan(inclination_rad) * H;
+        float X = H * Mathf.Cos(declination_rad);
+        float Y = H * Mathf.Sin(declination_rad);
+
+        return new Vector3(X, Y, Z);
+    }
 }
